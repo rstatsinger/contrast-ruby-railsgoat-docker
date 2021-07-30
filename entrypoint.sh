d6fe93728fa03dedff8a567701006dfd3430f231
Robert Statsinger <robert.statsinger@contrastsecurity.com>
2021-07-30 09:15:19 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 rm -f tmp/pids/server.pid
 export CONTRAST_CONFIG_PATH=/myapp/contrast_security.yaml
 export CONTRAST__APPLICATION__NAME=RailsGoatDocker
+export CONTRAST__AGENT__LOGGER__STDOUT=true
+export CONTRAST__AGENT__SERVICE_LOGGER__STDOUT=true
+export CONTRAST__ASSESS__ENABLE=true
+export CONTRAST__PROTECT__ENABLE=false
 
 bundle exec rails s -p 3000 -b '0.0.0.0'
